e3826f52078e77e8cc1a778f516f12e15581c367
TKDchampion <as1219123@yahoo.com.tw>
2025-02-21 12:11:12 +0800

    SSG

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,26 +1,17 @@
-"use client";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import Image from "next/image";
 
 const Banner: FC = () => {
-  const [imageUrl, setImageUrl] = useState("");
-
-  useEffect(() => {
-    setImageUrl(`https://picsum.photos/1920/1080`);
-  }, []);
-
   return (
     <div className="relative w-full min-h-screen flex items-center justify-center bg-black mb-16">
-      {imageUrl && (
-        <Image
-          src={imageUrl}
-          alt="Random Fullscreen Image"
-          layout="fill"
-          objectFit="cover"
-          priority
-          className="z-0"
-        />
-      )}
+      <Image
+        src="/images/banner.png"
+        alt="Random Fullscreen Image"
+        layout="fill"
+        objectFit="cover"
+        priority
+        className="z-0"
+      />
 
       <div className="absolute z-10 text-white text-center">
         <h1 className="text-4xl md:text-6xl font-bold">Welcome to My Site</h1>
